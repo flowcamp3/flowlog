79ae30da35fcf9590bf834123d7daa8e6671f2cd
YinaePark <dlsoso1@unist.ac.kr>
2023-07-14 16:56:49 +0900

    feat: _app, index

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import Layout from "../component/Layout";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
+}
