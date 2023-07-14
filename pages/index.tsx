e12b42953fe63553dcc02e8501f73f79b58b0779
fbre0717 <koh2040@naver.com>
2023-07-14 20:47:44 +0900

    chore: 카카오 인증 callback, 필요 없는 파일 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,18 @@
-// export default function MainPage() {
-//   return (
-//     <div>
-//       <h1>메인 페이지</h1>
-//       <button>카카오 로그인</button>
-//     </div>
-//   );
-// }
-
 import { signIn, useSession, signOut } from "next-auth/react";
 
 export default function MainPage() {
   const { data: session } = useSession();
 
   if (session) {
-    // console.log(session);
+    console.log(session.user.email);
+    console.log(session.user.name);
     return (
       <>
         <h1>메인 페이지</h1>
         {session.user?.name}님 반갑습니다 <br />
-        <button onClick={() => signOut()}>로그아웃</button>
+        <button onClick={() => signOut(
+          // { callbackUrl: "/" }
+          )}>로그아웃</button>
       </>
     );
   } else {
@@ -26,7 +20,9 @@ export default function MainPage() {
       <>
         <h1>메인 페이지</h1>
         로그인되지 않았습니다 <br />
-        <button onClick={() => signIn("kakao")}>카카오 로그인</button>
+        <button onClick={() => signIn("kakao"
+        // , { callbackUrl: "/blog" }
+        )}>카카오 로그인</button>
       </>
     );
   }
