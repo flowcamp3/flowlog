b9568094b5f3651eb9fa197052b4f84c82fa3a2f
YinaePark <dlsoso1@unist.ac.kr>
2023-07-15 15:58:23 +0900

    feat: 프로필 컴포넌트

--- component/UserProfile.tsx
+++ component/UserProfile.tsx
@@ -1,9 +1,43 @@
+import Image from "next/image";
+
 export default function UserProfile() {
   return (
-    <div>
-      <div>UserImg</div>
-      <div>UserNickname</div>
-      <div>UserInfo</div>
+    <div className={"container"}>
+      <div className="contents">
+        <div style={{ width: "80%" }}>
+          <Image
+            src="/assets/user_img.png"
+            alt="User Image"
+            layout="responsive"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className={"user_nickname"}>UserNickname</div>
+        <div className={"user_info"}>UserInfo</div>
+      </div>
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          width: 30vh;
+        }
+        .contents {
+          padding-top: 100px;
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          place-items: center;
+        }
+        .user_nickname {
+          padding-top: 30px;
+          font-size: 25px;
+          font-weight: 400;
+        }
+        .user_info {
+          padding-top: 20px;
+        }
+      `}</style>
     </div>
   );
 }
