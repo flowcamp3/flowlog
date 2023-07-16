f4fc594b969f68bfe99003ed6f7062d812816b51
YinaePark <dlsoso1@unist.ac.kr>
2023-07-17 01:40:37 +0900

    feat: 방명록 추가 삭제 기능

--- component/GuestbookBalloon.tsx
+++ component/GuestbookBalloon.tsx
@@ -1,34 +1,37 @@
+// writerid가 블로그 쿼리와 일치 시 말풍선 방향 바꾸는 코드 추가해야 함
+// 말풍선 옆에 user 아바타 놓는 코드 추가해야 함
+
 import React from "react";
 
 interface GuestbookBalloonProps {
   writerId: string;
   content: string;
+  onDelete: () => void;
 }
 
-// writerid가 블로그 쿼리와 일치 시 말풍선 방향 바꾸는 코드 추가해야 함
-// 말풍선 옆에 user 아바타 놓는 코드 추가해야 함
-
-export default function GuestbookBalloon({
+const GuestbookBalloon: React.FC<GuestbookBalloonProps> = ({
   writerId,
   content,
-}: GuestbookBalloonProps) {
+  onDelete,
+}) => {
   return (
-    <div>
-      <div className={"balloon"}>
-        <div className={"content_container"}>
-          <div className={"content"}>{content}</div>
-          <div className={"writer"}>{writerId}</div>
-        </div>
+    <div className="balloon">
+      <div className="content_container">
+        <div className="content">{content}</div>
+        <div className="writer">{writerId}</div>
+        <button className="delete_button" onClick={onDelete}>
+          X
+        </button>
       </div>
       <style jsx>{`
         .content_container {
-          padding: 25px;
+          padding: 50px 25px 25px 25px;
           display: inline-flex;
           flex-direction: column;
         }
         .content {
           display: inline-block;
-          padding-bottom: 25px;
+          padding-bottom: 20px;
         }
         .writer {
           color: var(--dark-green);
@@ -37,7 +40,6 @@ export default function GuestbookBalloon({
         .balloon {
           position: relative;
           display: inline-flex;
-
           margin: 50px;
           background: var(--light-text);
           border-radius: 10px;
@@ -50,10 +52,23 @@ export default function GuestbookBalloon({
           border-bottom: 0px solid transparent;
           content: "";
           position: absolute;
-          top: 10px;
+          bottom: 30px;
           left: -15px;
+        }
+        .delete_button {
+          position: absolute;
+          top: 10px;
+          right: 10px;
+          padding: 5px;
+          background-color: transparent;
+          border: none;
+          color: var(--dark-green);
+          font-size: 16px;
+          cursor: pointer;
         }
       `}</style>
     </div>
   );
-}
+};
+
+export default GuestbookBalloon;
--- pages/[blogId]/guestbook.tsx
+++ pages/[blogId]/guestbook.tsx
@@ -1,16 +1,71 @@
+import React, { useState } from "react";
 import BlogLayout from "./BlogLayout";
 import GuestbookBalloon from "../../component/GuestbookBalloon";
 
-export default function GuestBook() {
-  // 나중에 동적 처리를 위해 state 지정하거나 API 호출 해야 함
+interface GuestBookProps {}
+
+const GuestBook: React.FC<GuestBookProps> = () => {
   const writerId = "user1";
-  const content = "오랜만이다~ 밥 한번 먹자";
+  const [content, setContent] = useState("");
+  const [showInput, setShowInput] = useState(false);
+  const [contents, setContents] = useState<string[]>([]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  };
+
+  const handleInputSubmit = () => {
+    if (content.trim() !== "") {
+      setContents([...contents, content]);
+      setContent("");
+      setShowInput(false);
+    }
+  };
+
+  const handleShowInput = () => {
+    setShowInput(true);
+  };
+
+  const handleDelete = (index: number) => {
+    setContents((prevContents) => {
+      const updatedContents = [...prevContents];
+      updatedContents.splice(index, 1);
+      return updatedContents;
+    });
+  };
+
   return (
     <BlogLayout>
       <div className={"container"}>
         <div className={"guestbook_container"}>
           <h2 className={"title"}>GUESTBOOK</h2>
-          <GuestbookBalloon writerId={writerId} content={content} />
+          {contents.map((item, index) => (
+            <GuestbookBalloon
+              key={index}
+              writerId={writerId}
+              content={item}
+              onDelete={() => handleDelete(index)}
+            />
+          ))}
+          <div className={"input_container"}>
+            {showInput ? (
+              <div className="input_wrapper">
+                <button className={"send_button"} onClick={handleInputSubmit}>
+                  SEND MESSAGE
+                </button>
+                <textarea
+                  className={"input_textarea"}
+                  placeholder={"Enter your message..."}
+                  value={content}
+                  onChange={handleInputChange}
+                ></textarea>
+              </div>
+            ) : (
+              <button className={"input_button"} onClick={handleShowInput}>
+                INPUT
+              </button>
+            )}
+          </div>
         </div>
 
         <style jsx>{`
@@ -23,14 +78,48 @@ export default function GuestBook() {
             text-align: center;
           }
           .guestbook_container {
+            position: relative;
             width: 100%;
             height: 1000px;
             background-image: url("/assets/sample_header.jpg");
             background-size: cover;
             background-position: center;
           }
+          .input_container {
+            display: flex;
+            position: absolute;
+            bottom: 40px;
+            left: 40px;
+            align-items: center;
+            justify-content: flex-end;
+            margin-top: 10px;
+          }
+          .input_button,
+          .send_button {
+            font-size: 17px;
+            padding: 5px 10px;
+            background-color: var(--light-text);
+            border: none;
+            border-radius: 4px;
+            color: var(--dark-text);
+            cursor: pointer;
+          }
+          .input_wrapper {
+            display: flex;
+            align-items: center;
+          }
+          .input_textarea {
+            opacity: 0.8;
+            border: none;
+            resize: none;
+            width: 300px;
+            height: 80px;
+            padding: 5px;
+          }
         `}</style>
       </div>
     </BlogLayout>
   );
-}
+};
+
+export default GuestBook;
