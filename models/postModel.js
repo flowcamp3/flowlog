1f8ed89e2bf5ed39d30c764763bb6154983d0e00
fbre0717 <koh2040@naver.com>
2023-07-17 21:39:53 +0900

    feat: post 작성 기능 추가 DB연결

--- models/postModel.js
+++ models/postModel.js
@@ -5,12 +5,17 @@ const PostSchema = new mongoose.Schema({
   blogId: {
     type: String,
     required: true,
-    unique: true,
   },
-  postId: String,
+  postId: {
+    type: String,
+    required: true,
+  },
   title: String,
   date: String,
   content: String,
 });
+
+PostSchema.index({ blogId: 1, postId: 1 }, { unique: true });
+
 
 export default mongoose.models.Post || mongoose.model("Post", PostSchema);
---
+++ pages/api/posts/write.js
@@ -0,0 +1,37 @@
+// pages/api/auth/sign.js
+import connectMongo from "../../../utils/connectMongo";
+import Post from "../../../models/postModel";
+
+/**
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ */
+
+export default async function writehandler(req, res) {
+  console.log(req.body);
+  console.log(req.body);
+
+  if (req.method === "POST") {
+    // POST 요청이 오면 DB와 연결하고, user라는 collection에 document를 추가한다.
+    try {
+      await connectMongo();
+      
+      
+      const latestPost = await Post.find({ blogId: req.body.blogId }).sort({ _id: -1 }).limit(1);
+      const latestPostId = latestPost[0].postId;
+
+      console.log("GGGGGGGGGGGGGGGGGG")
+      console.log(latestPostId)
+
+      req.body.postId = (parseInt(latestPostId) + 1).toString();
+      const post = await Post.create(req.body);
+      res.status(200).json({ post });
+    } catch (error) {
+      console.log(error);
+      res.json({ error });
+    }
+  } else {
+    // 다른 메소드의 요청 처리
+    res.status(405).end();
+  }
+}
