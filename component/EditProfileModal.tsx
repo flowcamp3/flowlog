d25d4d3adc9eae47b51cecc9b3a5e2b61e56bab7
YinaePark <dlsoso1@unist.ac.kr>
2023-07-17 22:16:55 +0900

    fix: z index 수정

--- component/EditProfileModal.tsx
+++ component/EditProfileModal.tsx
@@ -1,5 +1,4 @@
 import React, { useRef, useEffect } from "react";
-import styles from "../styles/FriendModal.module.css";
 
 interface ModalProps {
   isOpen: boolean;
@@ -42,14 +41,46 @@ const EditProfileModal: React.FC<ModalProps> = ({
   };
 
   return (
-    <div className={styles.modalOverlay}>
-      <div className={styles.modalContent} ref={modalRef}>
-        <button className={styles.closeButton} onClick={handleModalClose}>
+    <div className={"modal_overlay"}>
+      <div className={"modal_content"} ref={modalRef}>
+        <button className={"close_btn"} onClick={handleModalClose}>
           &times;
         </button>
         <button onClick={handleModalClose}>X</button>
         {children}
       </div>
+
+      <style jsx>{`
+        .modal_overlay {
+          z-index: 10;
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(0, 0, 0, 0.5);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        .modal_content {
+          background-color: #fff;
+          padding: 20px;
+          border-radius: 4px;
+          max-width: 400px;
+        }
+
+        .close_btn {
+          position: absolute;
+          top: 10px;
+          right: 10px;
+          font-size: 24px;
+          background: transparent;
+          border: none;
+          cursor: pointer;
+        }
+      `}</style>
     </div>
   );
 };
