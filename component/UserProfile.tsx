bb97b37fa87a12566c482d986cc877f788df091c
YinaePark <dlsoso1@unist.ac.kr>
2023-07-17 18:36:15 +0900

    feat: 유저프로필 css 수정

--- component/UserProfile.tsx
+++ component/UserProfile.tsx
@@ -4,7 +4,7 @@ export default function UserProfile() {
   return (
     <div className={"container"}>
       <div className="contents">
-        <div style={{ width: "80%", border: "1px solid #ccc" }}>
+        <div className={"user_img"}>
           <Image
             src="/assets/user_img.png"
             alt="User Image"
@@ -13,14 +13,31 @@ export default function UserProfile() {
             height={100}
           />
         </div>
-        <div className={"user_nickname"}>UserNickname</div>
-        <div className={"user_info"}>UserInfo</div>
+        <div className={"lower_container"}>
+          <div className={"user_nickname"}>UserNickname</div>
+          <div className={"user_info"}>UserInfo</div>
+        </div>
       </div>
 
       <style jsx>{`
         .container {
           width: 30%;
-          border: 1px solid #ccc;
+          background-color: var(--light-text);
+          background-image: radial-gradient(
+            circle at 40% 40%,
+            var(--light-green) 0%,
+            var(--light-green) 20%,
+            transparent 20%,
+            transparent 100%
+          );
+          background-size: 20px 20px;
+          
+          );
+        }
+        .user_img {
+          width: 60%;
+          border: 5px solid var(--dark-green);
+          box-shadow: 10px 10px var(--dark-green);
         }
         .contents {
           padding-top: 100px;
@@ -29,13 +46,32 @@ export default function UserProfile() {
           flex-direction: column;
           place-items: center;
         }
+
+        .lower_container{
+          margin-top: 40px;
+          width: 70%;
+          height: 400px;
+          background-color: white;
+          border: 5px solid var(--dark-green);
+          box-shadow: 10px 10px var(--dark-green);
+          display: flex;
+          flex-direction: column;
+          place-items: center;
+
+        }
         .user_nickname {
-          padding-top: 30px;
+          margin-top: 15px;
+          margin-bottom: 5px;
+          color: var(--dark-green);
           font-size: 25px;
           font-weight: 400;
+          text-decoration: underline;
+          text-decoration-style: dotted;
+          text-decoration-skip-ink: none;
         }
         .user_info {
           padding-top: 20px;
+          color: var(--dark-green);
         }
       `}</style>
     </div>
