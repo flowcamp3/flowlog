8ba973f963418518bbace5ad5fe9e12979952e04
YinaePark <dlsoso1@unist.ac.kr>
2023-07-19 01:49:33 +0900

    feat: 포스트 탭 스타일 수정

--- pages/[blogId]/[postId].tsx
+++ pages/[blogId]/[postId].tsx
@@ -12,16 +12,15 @@ const PostPage: React.FC<PostProps> = ({ post }) => {
   return (
     <BlogLayout>
       <div className="container">
-        <div className="container2">
-          <div className="title">{post.title}</div>
-          <div className="date">{post.date}</div>
-          <div className="content">{post.content}</div>
+        <div className="background_container">
+          <div className="container2">
+            <div className="title">{post.title}</div>
+            <div className="date">{post.date}</div>
+            <div className="content">{post.content}</div>
+          </div>
         </div>
       </div>
       <style jsx>{`
-        form {
-          display: block;
-        }
         .container {
           border: 1px solid #ccc;
           display: flex;
@@ -29,25 +28,48 @@ const PostPage: React.FC<PostProps> = ({ post }) => {
           width: 70%;
           margin: 0 auto;
         }
+        .background_container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 1000px;
+          background-image: url("/assets/sample_header.jpg");
+          background-size: cover;
+          background-position: center;
+        }
         .container2 {
           border: 1px solid #ccc;
-          border-radius: 10px;
-          // padding: 10px;
-          margin: 10px;
+          border-radius: 33px;
+          background-color: rgba(255, 255, 255, 0.1);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          width: 80%;
+          height: 80%;
+          box-shadow: -8px -8px 10px rgba(255, 255, 255, 0.2),
+            8px -8px 10px rgba(255, 255, 255, 0.2),
+            -8px 8px 10px rgba(255, 255, 255, 0.2),
+            8px 8px 10px rgba(255, 255, 255, 0.2);
         }
         .title {
-          font-size: 40px;
-          margin: 10px;
+          font-size: 30px;
           margin-bottom: 20px;
+          color: white;
         }
         .date {
           margin: 10px;
           font-size: 20px;
           text-align: right;
+          color: var(--light-text);
         }
         .content {
-          margin: 10px;
+          height: 60%;
+          color: white;
+          margin-top: 80px;
           font-size: 20px;
+          text-align: center; /* Added to center the content */
         }
       `}</style>
     </BlogLayout>
