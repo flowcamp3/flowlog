518a5d44ffc10be29f03de5ea4b456a3a0ba8183
fbre0717 <koh2040@naver.com>
2023-07-18 18:28:56 +0900

    feat: followingModel

--- component/TopNavBar.tsx
+++ component/TopNavBar.tsx
@@ -4,12 +4,27 @@ import { useRouter } from "next/router";
 import FriendModal from "./FriendModal";
 import { signOut, useSession } from "next-auth/react";
 
+interface FollowingData {
+  blogId: string;
+  blogName: string;
+}
+
+const allFollwing = (): FollowingData[] => {
+  return [
+    { blogId: "koh2040@naver.com", blogName: "panorama" },
+    { blogId: "iineaya@naver.com", blogName: "ihihihi" },
+    { blogId: "abcdefg@naver.com", blogName: "친구3" },
+  ];
+};
+
 export default function TopNavBar() {
   const router = useRouter();
   const [modalOpen, setShowModal] = useState(false);
+  const [allFollwingData, setAllFollowingData] = useState<FollowingData[]>([]);
   const { data: session } = useSession();
   const openModal = () => {
     setShowModal(true);
+    setAllFollowingData(allFollwing());
   };
 
   const closeModal = () => {
@@ -47,10 +62,6 @@ export default function TopNavBar() {
               >
                 친구 목록
               </div>
-              <FriendModal isOpen={modalOpen} onClose={closeModal}>
-                <h2>친구 목록</h2>
-                <p>모달 내용이 들어갑니다</p>
-              </FriendModal>
               <div
                 style={{ marginRight: "20px", cursor: "pointer" }}
                 className="btn"
@@ -60,6 +71,18 @@ export default function TopNavBar() {
               >
                 로그아웃
               </div>
+              <FriendModal isOpen={modalOpen} onClose={closeModal}>
+                <h2>친구 목록</h2>
+                <ul className={"list"}>
+                  {allFollwingData.map(({ blogId, blogName }) => (
+                    <li className={"listItem"} key={blogId}>
+                      <a href={`/${blogId}`} onClick={closeModal}>
+                        {blogName}({blogId})
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </FriendModal>
             </>
           ) : (
             <Link href="/signin">
@@ -96,6 +119,18 @@ export default function TopNavBar() {
           }
           .active {
             text-decoration: none;
+          }
+          .list {
+            list-style: none;
+            padding: 10px;
+            margin: 0;
+          }
+          .listItem {
+            border: 1px solid #ccc;
+            margin: 0 0 1.25rem;
+            font-size: large;
+            border-radius: 10px;
+            padding: 10px;
           }
         `}</style>
       </nav>
